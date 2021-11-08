193116dc6a1a0a8413941335c6f37d68e8e989fd
OCANDABEAST <81494306+OCANDABEAST@users.noreply.github.com>
2021-11-07 21:58:16 -0500

    Add files via upload
    Added: 
    Java Script 
    Voice Command 
    Image Recognition (Mobile Net)

--- main.js
+++ main.js
@@ -1,86 +1,42 @@
-
-    function preload() {
-classifier = ml5.imageClassifier('DoodleNet');
-}
-    
-    function clearCanvas() {
-    
-    background("white");
-    }
-
-  function draw() {
-  
-  // Set stroke weight to 13
-  strokeWeight(13);
-  // Set stroke color to black
-  stroke(0);
-  // If mouse is pressed, draw line between previous and current mouse positions
-  if (mouseIsPressed) {
-  line(pmouseX, pmouseY, mouseX, mouseY);
-  }}
-
-  function classifyCanvas() {
-    classifier.classify(canvas, gotResult);
-  }
-
-  function gotResult(error, results) {
-if (error) {
-console.error(error);
-}
-console.log(results);
-document.getElementById('label').innerHTML = 'label: '+ results[0].label;
-
-document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
-utterThis = new SpeechSynthesisUtterance(results[0].label);
-synth.speak(utterThis);
-}
-
-
-
-function updateCanvas() {
-  background("white");
-  random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
-  console.log(quick_draw_data_set[random_number]);
-  sketch = quick_draw_data_set[random_number];
-  document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
-}
-
+var previous_result = '';
 
 function setup() {
-  canvas = createCanvas(280, 280);
+  canvas = createCanvas(300, 300);
   canvas.center();
-  background("white");
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
 }
 
+function modelLoaded() {
+console.log('Model Loaded!');
+}
 
 function draw() {
-  check_sketch()
-  if(drawn_sketch == sketch)
+image(video, 0, 0, 300, 300);
+classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results) {
+if (error) {
+console.error(error);
+} else {
+  if((results[0].confidence > 0.5) && (previous_result != results[0].label))
   {
-    answer_holder = "set"
-    score++;
-    document.getElementById('score').innerHTML = 'Score: ' + score;
+  console.log(results);
+  previous_result = results[0].label;
+  var synth = window.speechSynthesis;
+  speak_data = 'Object detected is - '+results[0].label;
+  var utterthis = new SpeechSynthesisUtterance(speak_data);
+  synth.speak(utterthis);
+
+
+  document.getElementById("result_object_name").innerHTML = results[0].label;
+  document.getElementById("results_object_accuracy").innerHTML = results [0].confidence.toFixed(3);
   }
-
+}
 }
 
 
-function check_sketch()
-{
-  timer_counter++;
-  document.getElementById('time').innerHTML = 'Timer: ' + timer_counter;
-  console.log(timer_counter)
-  if(timer_counter > 400)
-    {
-      timer_counter = 0;
-      timer_check = "completed"
-    }
-    if(timer_check =="completed" || answer_holder == "set")
-    {
-      timer_check = "";
-      answer_holder = "";
-      updateCanvas();
-    }
 
-}
 
